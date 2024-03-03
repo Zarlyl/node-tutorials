0c2448aa196e640fb7350f76960027a735ddcfda
Zarlyl <msstephens03@gmail.com>
2024-03-03 17:40:39 -0500

    Unworking commit. Working on using environment variables.

--- node-crash-course/app.js
+++ node-crash-course/app.js
@@ -3,11 +3,15 @@ const morgan = require('morgan');
 const mongoose = require('mongoose');
 const blogRoutes = require('./routes/blogRoutes')
 
+// environment variables
+require('dotenv').config();
+
 // express app
 const app = express();
 
 // connect to mongodb
-const dbURI = 'mongodb+srv://msstephens03:deiZt6z6iSCdDm3e@nodetuts.xw3mnxv.mongodb.net/nodetuts?retryWrites=true&w=majority&appName=nodetuts';
+console.log('This is the db URI: ' + process.env.DB_URI);
+const dbURI = process.env.DB_URI;
 mongoose.connect(dbURI)
     .then((result) => app.listen(3000))
     .catch((err) => console.log(err));
@@ -17,7 +21,7 @@ app.set('view engine', 'ejs');
 
 // middleware & static files
 app.use(express.static('public'));
-app.use(express.urlencoded( {extended: true } )); //takes url encoded data and passes it into an obj for post requests for form data
+app.use(express.urlencoded( { extended: true } )); //takes url encoded data and passes it into an obj for post requests for form data
 app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
